900e85c11ccb84a289908f30287d792c417e6d26
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-13 16:21:03 +0100

    Static form added

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-var_dump("ciao");
+
 
 ?>
 
@@ -12,8 +12,21 @@ var_dump("ciao");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
 <body>
     
+    <div class="container my-5">
+       <form action="" method="GET">
+            <div class="mb-3">
+              <label for="psw_length" class="form-label">Password Generator</label>
+              <input type="text" class="form-control" id="psw_length" placeholder="Enter Password Length" name="psw_length">
+            </div> 
+              <input type="button" value="Generate Password">
+        </form>
+    </div>
+
 </body>
 </html>
